ddb35e8f33f5aa75ce61acd1c844011c5871ad5d
likith sai <likithsai13@gmail.com>
2023-08-16 16:56:41 +0530

    code refactor

--- src/app/pages/item-detail/item-detail.component.ts
+++ src/app/pages/item-detail/item-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { ItemState } from 'src/app/store/app.state';
 
 @Component({
   selector: 'app-item-detail',
@@ -8,10 +10,22 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class ItemDetailComponent {
   query: Params = {};
+  productItems: ItemState | undefined;
 
-  constructor(private activeRoute: ActivatedRoute) {
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private store: Store<{ items: ItemState }>
+  ) {
     this.activeRoute.queryParams.subscribe((qp) => {
       this.query = this.activeRoute.snapshot.params;
     });
+  }
+
+  ngOnInit() {
+    this.store
+      .select((state) => state.items)
+      .subscribe((items) => {
+        this.productItems = items;
+      });
   }
 }
